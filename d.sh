a4f407c53a9b11543f5a4b7c6a6bd93b256137a4
leogabriele <jagan1208nadar@gmail.com>
2022-05-07 17:55:11 +0530

    Added: file4

---
+++ d.sh
@@ -0,0 +1,13 @@
+# Sum & Avg
+
+num1=$((15))
+num2=$((20))
+num3=$((25))
+num4=$((30))
+num5=$((35))
+
+sum=$((num1+num2+num3+num4+num5))
+avg=$((sum/5))
+
+echo "Sum is: " $sum
+echo "Average is: " $avg
